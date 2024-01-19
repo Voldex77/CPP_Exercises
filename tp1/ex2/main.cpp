a504332b7606294b829e1dcac813366475119a6a
Julien VU <julien.vu@etud.univ-eiffel.fr>
2024-01-19 12:34:21 +0100

    Modification du main pour la question 6

--- tp1/ex2/main.cpp
+++ tp1/ex2/main.cpp
@@ -1,9 +1,31 @@
 #include "Person.hpp"
 #include <iostream>
-
+#include <vector>
+#include <sstream>
 int main()
 {
-    Person person{"Palluche", "La Faluche"};
-    std::cout << person.getFirstName() << " " << person.getSurname() << std::endl;
+    int numbers;
+    std::cout << "Nombre de personnes: ";
+    std::cin >> numbers;
+
+    auto persons = std::vector<Person> {};
+    std::string fn, sn;
+    for (auto i = 0; i < numbers; i++)
+    {
+        std::cout << "Prenom: ";
+        std::cin >> fn;
+        std::cout << "Nom: ";
+        std::cin >> sn;
+        persons.emplace_back(fn, sn);
+    }
+    std::stringstream builder;
+    builder << "Les personnes sont";
+    for (auto p: persons)
+    {
+        builder << " " << p.getFirstName() << ", " << p.getSurname();
+    }
+    std::cout << builder.str() << "." << std::endl;
     return 0;
 }
+
+// ici, pour déclarer un tableau de Person, on est obligé de passer par auto avec vector.
