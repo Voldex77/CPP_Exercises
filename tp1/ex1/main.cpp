0b20a2bee20775a7e1f9720fe296f424baebe092
Julien VU <julien.vu@etud.univ-eiffel.fr>
2024-01-19 11:19:07 +0100

    Modification du type de name

--- tp1/ex1/main.cpp
+++ tp1/ex1/main.cpp
@@ -5,8 +5,8 @@ int main()
 {
     std::cout << "Entre ton nom: ";
 
-    char name[20] = "";
-    std::cin >> std::setw(20) >> name;
+    std::string name;
+    std::cin >> name;
     std::cout << "Bonjour " << name << " !" << std::endl;
 
     return 0;
