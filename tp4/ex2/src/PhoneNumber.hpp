6451b3a512dcc258512b113423b0e05bb49a2daa
Julien VU <julien.vu@etud.univ-eiffel.fr>
2024-02-09 11:48:47 +0100

    Ex2-q2

--- tp4/ex2/src/PhoneNumber.hpp
+++ tp4/ex2/src/PhoneNumber.hpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <vector>
 
 class PhoneNumber
 {
     public:
         PhoneNumber(int v1, int v2, int v3, int v4, int v5): _v1 {v1}, _v2 {v2}, _v3 {v3}, _v4 {v4}, _v5 {v5} {};
+        bool is_valid()
+        {
+            auto values = std::vector<int> { _v1, _v2, _v3, _v4, _v5 };
+            for (auto v: values)
+            if (0 > v || v > 99)
+            {
+                return false;
+            }
+            return true;
+        }
 
     private:
         int _v1;
