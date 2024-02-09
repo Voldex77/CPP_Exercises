3c6c6257aaf79cc1d999e70a028f2bd268930edc
Julien VU <julien.vu@etud.univ-eiffel.fr>
2024-02-09 11:38:36 +0100

    ex2-q1

---
+++ tp4/ex2/src/PhoneNumber.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class PhoneNumber
+{
+    public:
+        PhoneNumber(int v1, int v2, int v3, int v4, int v5): _v1 {v1}, _v2 {v2}, _v3 {v3}, _v4 {v4}, _v5 {v5} {};
+
+    private:
+        int _v1;
+        int _v2;
+        int _v3;
+        int _v4;
+        int _v5;
+
+};
