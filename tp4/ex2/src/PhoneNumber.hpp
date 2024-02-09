017b0706f67540fa409bf9026a1ae4c1992778ae
Julien VU <julien.vu@etud.univ-eiffel.fr>
2024-02-09 12:10:48 +0100

    ex2-q3 et q4 (optimisation du code)

--- tp4/ex2/src/PhoneNumber.hpp
+++ tp4/ex2/src/PhoneNumber.hpp
@@ -4,23 +4,26 @@
 class PhoneNumber
 {
     public:
-        PhoneNumber(int v1, int v2, int v3, int v4, int v5): _v1 {v1}, _v2 {v2}, _v3 {v3}, _v4 {v4}, _v5 {v5} {};
-        bool is_valid()
+        PhoneNumber(int v1, int v2, int v3, int v4, int v5)
         {
-            auto values = std::vector<int> { _v1, _v2, _v3, _v4, _v5 };
-            for (auto v: values)
-            if (0 > v || v > 99)
+            v = std::vector<int> { v1, v2, v3, v4, v5 };
+        };
+        bool is_valid() const
+        {
+            for (auto e: v)
+            if (0 > e || e > 99)
             {
                 return false;
             }
             return true;
         }
 
-    private:
-        int _v1;
-        int _v2;
-        int _v3;
-        int _v4;
-        int _v5;
+        int operator[] (int index)
+        {
+            return v[index];
+        }
 
+
+    private:
+        std::vector<int> v;
 };
