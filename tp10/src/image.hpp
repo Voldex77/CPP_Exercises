c5a0de8c88ae7e522c9521c06ab95ce7548f14b1
Julien VU <julien.vu@etud.univ-eiffel.fr>
2024-04-04 11:35:38 +0200

    Exo1 template class Image (q3)

---
+++ tp10/src/image.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+// UTILISEZ --std=c++17
+
+template <typename P, size_t W, size_t H>
+class Image
+{
+    public:
+        Image(const P& pixel) {
+            for(auto &line: _pixels) {
+                for (auto &p: line) {
+                    p = pixel;
+                }
+            }
+        }
+        Image() = default;
+
+        P& operator()(const size_t i, const size_t j) {
+            return _pixels[j][i];
+        }
+        const P& operator()(const size_t i, const size_t j) {
+            return _pixels[j][i];
+        }
+
+        
+    private:
+        std::array<std::array<P, W>, H> _pixels;
+};
