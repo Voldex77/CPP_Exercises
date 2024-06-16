3ec4758ff2e17c250fb600098f72f2d1c4c6f108
Voldex <julienvu77@gmail.com>
2024-06-17 00:24:13 +0200

    TP8 ex3-3

--- tp8/ex3/src/DerivedInt.hpp
+++ tp8/ex3/src/DerivedInt.hpp
@@ -1,10 +1,14 @@
 #pragma once
 
-class DerivedInt{
+class DerivedInt: public Base {
     public:
         DerivedInt(int n): _n {n} {}
         int data() const {
             return _n;
+        }
+
+        bool is_null() const override {
+            return _n == 0;
         }
     private:
         int _n;
--- tp8/ex3/src/DerivedString.hpp
+++ tp8/ex3/src/DerivedString.hpp
@@ -2,11 +2,15 @@
 
 #include <string>
 
-class DerivedString {
+class DerivedString: public Base {
     public:
         DerivedString(const std::string& str): _str {str} {}
         const std::string& data() const {
             return _str;
+        }
+
+        bool is_null() const override {
+            return _str == "";
         }
     private:
         std::string _str;
