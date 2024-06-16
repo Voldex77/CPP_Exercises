e210e958beb5a6500a86367e898c49264b1ba403
Voldex <julienvu77@gmail.com>
2024-06-16 10:07:01 +0200

    TP5 B2-C3

--- tp5/2-ptr/CopyablePtr.hpp
+++ tp5/2-ptr/CopyablePtr.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Object.hpp"
+#include <utility>
 
 class CopyablePtr
 {
@@ -9,7 +10,17 @@ class CopyablePtr
 
         CopyablePtr(int n): _obj { new Object(n) } {}
 
-        CopyablePtr(const CopyablePtr& other): _obj { new Object(*other._obj) } {}
+        CopyablePtr(const CopyablePtr& other) {
+            if (other == nullptr) {
+                _obj = nullptr;
+            } else {
+                _obj = new Object(*other._obj);
+            }
+        }
+
+        CopyablePtr(CopyablePtr&& other): _obj { std::move(other._obj) } {
+            other._obj = nullptr;
+        }
 
         bool operator==(const std::nullptr_t& ptr) const {
             return _obj == ptr;
@@ -22,6 +33,27 @@ class CopyablePtr
         void operator=(std::nullptr_t newobj) {
             delete _obj;
             _obj = newobj;
+        }
+
+        CopyablePtr& operator=(const CopyablePtr& other) {
+            if (this != &other) {
+                if (other._obj != nullptr) {
+                    delete _obj;
+                }
+                _obj = new Object(*other._obj);
+            }
+            return *this;
+        }
+
+        CopyablePtr& operator=(CopyablePtr&& other) {
+            if (this != &other) {
+                if (other._obj != nullptr) {
+                    delete _obj;
+                }
+                _obj = other._obj;
+                other._obj = nullptr;
+            }
+            return *this;
         }
     private:
         Object* _obj;
